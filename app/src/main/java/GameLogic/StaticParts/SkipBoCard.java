f879777a270441e4f0e576bf343029861ff2be2d
Admir Dizdarevic <cpt@pop-os.localdomain>
2023-04-17 15:30:38 +0200

    Removed instance count to avoid initial structure  confusion

--- app/src/main/java/GameLogic/StaticParts/SkipBoCard.java
+++ app/src/main/java/GameLogic/StaticParts/SkipBoCard.java
@@ -4,19 +4,9 @@ package GameLogic.StaticParts;
  * This class ensures that no more than 12 instances of SkipBoCard can be created.
  */
 public class SkipBoCard extends GameCard{
-    /** The number of instances of SkipBoCard that have been created. */
-    private static int instanceCount = 0;
 
-
-    /**
-     * Constructs a new SkipBoCard instance.
-     * @throws IllegalStateException if more than 12 instances of SkipBoCard have been created.
-     */
 
     public SkipBoCard() {
-        if (instanceCount >= 12) {
-            throw new IllegalStateException("Only 12 instances of SkipBoCard can be created.");
-        }
-        instanceCount++;
+
     }
 }
