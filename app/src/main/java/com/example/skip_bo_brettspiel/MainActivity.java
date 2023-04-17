a3fe9c915f76f36636992edbc9defc2dd5f3dc9d
Tobias Wernig <towernig@edu.aau.at>
2023-04-17 23:03:24 +0200

    added the ability to select a username for the current player

--- app/src/main/java/com/example/skip_bo_brettspiel/MainActivity.java
+++ app/src/main/java/com/example/skip_bo_brettspiel/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.skip_bo_brettspiel;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.skip_bo_brettspiel.GameModel.Player;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        EditText usernameField = findViewById(R.id.editUsername);
+        Player currentPlayer = new Player(usernameField.getText().toString());
+
+        // textWatcher is for watching any changes in editText
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // this function is called when text is edited
+                currentPlayer.setName(usernameField.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+        };
+
+        usernameField.addTextChangedListener(textWatcher);
     }
 }
--- app/src/test/java/com/example/skip_bo_brettspiel/GameModel/PlayerUnitTest.java
+++ app/src/test/java/com/example/skip_bo_brettspiel/GameModel/PlayerUnitTest.java
@@ -8,7 +8,7 @@ public class PlayerUnitTest {
 
     @Test
     void test() {
-        Player player = new Player();
+        Player player = new Player("Player");
         player.cards.add(new Card(0));
 
         assertEquals(0, player.cards.get(0).value);
