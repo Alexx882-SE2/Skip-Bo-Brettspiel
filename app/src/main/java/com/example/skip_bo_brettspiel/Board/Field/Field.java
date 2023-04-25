641443731470f83a46728064b0e23d38ad883eca
Admir Dizdarevic <cpt@pop-os.localdomain>
2023-04-25 02:16:13 +0200

    Added docs for classes

--- app/src/main/java/com/example/skip_bo_brettspiel/Board/Field/Field.java
+++ app/src/main/java/com/example/skip_bo_brettspiel/Board/Field/Field.java
@@ -1,15 +1,32 @@
 package com.example.skip_bo_brettspiel.Board.Field;
 
 public abstract class Field {
-
-    private String name;
+    /**
+      The name of the field.
+     */
+     private String name;
+     /*
+     * The position of the field on the board.
+     */
     private int positionOnBoard;
 
-    public Field(String name, int value) {
+    /**
+     * Constructs a Field object with the given name and position.
+     *
+     * @param name The name of the field.
+     * @param positionOnBoard The position of the field on the board.
+     */
+    public Field(String name, int positionOnBoard) {
+        this.name = name;
+        this.positionOnBoard = positionOnBoard;
     }
 
-
-    public String description(){
+    /**
+     * Returns a description of the field, including its name and position on the board.
+     *
+     * @return A description of the field.
+     */
+    public String description() {
         return name + " " + positionOnBoard;
     }
 
