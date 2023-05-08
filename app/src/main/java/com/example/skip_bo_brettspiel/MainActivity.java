19e94366cdb2be38b45bfaa8bb9a474ec0b42559
wernigtobias <121678949+wernigtobias@users.noreply.github.com>
2023-05-09 00:33:59 +0200

    Revert "feat(network): Simple Network Implementation"

--- app/src/main/java/com/example/skip_bo_brettspiel/MainActivity.java
+++ app/src/main/java/com/example/skip_bo_brettspiel/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.skip_bo_brettspiel;
 
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,21 +18,10 @@ import com.example.skip_bo_brettspiel.Board.Field.SkipBoField;
 import com.example.skip_bo_brettspiel.Board.Field.SwitchCardField;
 import com.example.skip_bo_brettspiel.GameModel.Game;
 import com.example.skip_bo_brettspiel.GameModel.Player;
-import com.example.skip_bo_brettspiel.api.GameClient;
-import com.example.skip_bo_brettspiel.api.GameServer;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final ArrayList<Player> players = new ArrayList<>();
-
-    public static boolean gameCreated = false;
-
-    private static GameServer gameServer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,10 +31,8 @@ public class MainActivity extends AppCompatActivity {
         EditText usernameField = findViewById(R.id.editUsername);
         Button createGameButton = findViewById(R.id.createButton);
         Button joinGameButton = findViewById(R.id.joinButton);
+        Player currentPlayer = new Player(usernameField.getText().toString());
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-      
         createGameButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 currentPlayer.setName(usernameField.getText().toString());
@@ -100,45 +85,16 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-        createGameButton.setOnClickListener(v -> {
-            if (!gameCreated) {
-                String playerName = usernameField.getText().toString();
-                if (!playerName.isEmpty()) {
-                    Player player = new Player(playerName, "");
-                    try {
-                        gameServer = new GameServer(players);
-                        new GameClient(player, MainActivity.this);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Please enter a player name.", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(MainActivity.this, "Already active lobby", Toast.LENGTH_SHORT).show();
             }
         });
 
-        joinGameButton.setOnClickListener(v -> {
-            if (gameCreated) {
-                String playerName = usernameField.getText().toString();
-                if (!playerName.isEmpty()) {
-                    Player player = new Player(playerName, "");
-                    try {
-                        if (gameServer.getNumPlayers() > 3) {
-                            Toast.makeText(MainActivity.this, "There are already 4 players in the lobby!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            new GameClient(player, MainActivity.this);
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Please enter a player name.", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(MainActivity.this, "No active lobby.", Toast.LENGTH_SHORT).show();
+        joinGameButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                currentPlayer.setName(usernameField.getText().toString());
+                //Code to join existing game.
+
             }
         });
+
     }
 }
